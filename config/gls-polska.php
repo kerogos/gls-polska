2a037e7b140ac21d9164552eb675672a3252e37f
kerog <konrad.github@kerogos.pl>
2026-05-04 21:24:58 +0200

    Fix config and wsdl address

--- config/gls-polska.php
+++ config/gls-polska.php
@@ -1,9 +1,18 @@
 <?php
 
 
+$sandbox = env('GLS_ADE_SANDBOX', false);
+
 return [
-	'isSandbox' => env('GLS_ADE_SANDBOX', false),
-	'wsdl' => env('GLS_ADE_WSDL', config('gls-polska.isSandbox') ? 'https://ade-test.gls-poland.com/adeplus/pm1/ade_webapi2.php?wsdl' :'https://adeplus.gls-poland.com/adeplus/pm1/webapi2.asmx?WSDL'),
+	'isSandbox' => $sandbox,
+	
+	'wsdl' => env(
+		'GLS_ADE_WSDL',
+		$sandbox
+			? 'https://ade-test.gls-poland.com/adeplus/pm1/ade_webapi2.php?wsdl'
+			: 'https://adeplus.gls-poland.com/adeplus/pm1/ade_webapi2.php?wsdl'
+	),
+	
 	
 	'username'          => env('GLS_ADE_USERNAME'),
 	'password'          => env('GLS_ADE_PASSWORD'),
